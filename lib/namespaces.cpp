9d53c6442f7ce8fa6d4ca206c034c91e39584f32
sitb157 <sitb157@gmail.com>
2023-05-25 00:12:07 +0900

    [Add] Namespace

---
+++ lib/namespaces.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "namespaces.h"
+
+void mycode::foo() {
+    std::cout << "foo() called in the mycode namespaces" << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include "shape.h"
 #include <iostream>
+#include "shape.h"
+#include "namespaces.h" 
+
+using namespace mycode;
 
 int main() {
+  foo();
   Rectangle rectangle(20, 30);
   std::cout << "rectangle size : " << rectangle.GetSize() << std::endl;
   return 0;
